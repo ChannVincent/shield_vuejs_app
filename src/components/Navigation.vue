973c68afcbbdf3e540826a7b4c9de99206ceec7e
Vincent Chann <>
2024-12-29 13:55:56 +0100

    ui

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,28 +1,10 @@
 <template>
     <header class="sticky top-0 bg-white shadow-lg">
-      <nav class="container flex flex-row items-center gap-4 text-primary h-20">
-          <div 
-          class="rounded-full bg-primary hover:bg-secondary text-white"
-          style="width:45px;height:45px;text-align:center;"
-          >
-            <i 
-            class="fa-solid fa-arrow-up-wide-short text-md"
-            style="width:17px;height:17px;margin:14px;"
-            />
-          </div>
+      <nav class="container flex flex-row items-center gap-4 text-blue-500 h-20">
           <div class="m-auto">
             <RouterLink :to="{ name: 'PostListView' }">
                   <p class="text-2xl">Bussy Saint Georges</p>
             </RouterLink>
-          </div>
-          <div 
-          class="rounded-full bg-primary hover:bg-secondary text-white"
-          style="width:45px;height:45px;text-align:center;"
-          >
-            <i 
-            class="fa-solid fa-magnifying-glass text-md"
-            style="width:17px;height:17px;margin:14px;"
-            />
           </div>
       </nav>
     </header>
